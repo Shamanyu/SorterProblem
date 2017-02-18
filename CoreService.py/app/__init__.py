10833f23e4588db83bccf9c88c67da9377de9657
shamanyu <shubhamshamanyu@gmail.com>
2017-02-19 01:20:20 +0530

    Implement core service

--- CoreService.py/app/__init__.py
+++ CoreService.py/app/__init__.py
@@ -2,3 +2,43 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 from app import views
+
+class Packet():
+  
+  def __init__():
+    self.packet_data = dict()
+  
+  def set_barcode(barcode):
+    self.packet_data.barcode = barcode
+
+  def get_barcode():
+    return self.packet_data.barcode
+
+  def set_weight(weight):
+    self.packet_data.weight = weight
+
+  def get_weight():
+     return self.packet_data.weight
+
+  def set_arm(arm):
+    self.packet_data.arm = arm
+
+  def get_arm():
+    return self.packet_data.arm
+
+class Sorter():
+
+  def __init__():
+    self.camera_sensor_id = 0
+    self.weight_sensor_id = 0
+    self.arm_sensor_id = 0
+    self.packets = list()
+
+  def add_packet(packet):
+    packet.camera_sensor = False
+    packet.weight_sensor = False
+    packet.arm_sensor = False
+    self.packets.append(packet)
+
+  def get_packets():
+    return self.packets
--- CoreService.py/app/views.py
+++ CoreService.py/app/views.py
@@ -11,3 +11,15 @@ def camera_sensor():
   url = app.config['CONNECTION_PROTOCOL']+app.config['BARCODE_SERVICE_URL']+app.config['BARCODE_SERVICE_PORT']+app.config['BARCODE_SERVICE_ENDPOINT']
   headers = {}
   return str((requests.get(url, headers=headers)).json())
+
+@app.route('/weight_sensor')
+def weight_sensor():
+  url = app.config['CONNECTION_PROTOCOL']+app.config['WEIGHT_SERVICE_URL']+app.config['WEIGHT_SERVICE_PORT']+app.config['WEIGHT_SERVICE_ENDPOINT']
+  headers = {}
+  return str((requests.get(url, headers=headers)).json())
+
+@app.route('/arm_sensor')
+def arm_sensor():
+  url = app.config['CONNECTION_PROTOCOL']+app.config['ARM_SERVICE_URL']+app.config['ARM_SERVICE_PORT']+app.config['ARM_SERVICE_ENDPOINT']
+  headers = {}
+  return str((requests.get(url, headers=headers)).json())
