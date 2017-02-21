62dc962cf63560d4e56300a57b233ab474f16a4c
shamanyu <shubhamshamanyu@gmail.com>
2017-02-21 22:08:49 +0530

    Add methods to support packet flow through sorter

--- CoreService.py/app/__init__.py
+++ CoreService.py/app/__init__.py
@@ -5,40 +5,68 @@ from app import views
 
 class Packet():
   
-  def __init__():
+  def __init__(self):
     self.packet_data = dict()
   
-  def set_barcode(barcode):
+  def set_barcode(self, barcode):
     self.packet_data.barcode = barcode
 
-  def get_barcode():
+  def get_barcode(self):
     return self.packet_data.barcode
 
-  def set_weight(weight):
+  def set_weight(self, weight):
     self.packet_data.weight = weight
 
-  def get_weight():
+  def get_weight(self):
      return self.packet_data.weight
 
-  def set_arm(arm):
+  def set_arm(self, arm):
     self.packet_data.arm = arm
 
-  def get_arm():
+  def get_arm(self):
     return self.packet_data.arm
+
+app.PacketService = Packet()
 
 class Sorter():
 
-  def __init__():
-    self.camera_sensor_id = 0
-    self.weight_sensor_id = 0
-    self.arm_sensor_id = 0
+  def __init__(self):
+    self.camera_sensor_packets_detected = 0
+    self.weight_sensor_packets_detected = 0
+    self.arm_sensor_packets_detected = 0
     self.packets = list()
 
-  def add_packet(packet):
+  def add_packet(self, packet):
     packet.camera_sensor = False
     packet.weight_sensor = False
     packet.arm_sensor = False
     self.packets.append(packet)
 
-  def get_packets():
+  def update_packet(self, packet_number, packet):
+    self.packets[packet_number] = packet
+
+  def get_packet(self, packet_number):
+    return self.packets[packet_number]
+
+  def get_packets(self):
     return self.packets
+
+  def update_camera_sensor_packets_detected(self):
+    self.camera_sensor_packets_detected = self.camera_sensor_packets_detected + 1
+
+  def get_camera_sensor_packets_detected(self):
+    return self.camera_sensor_packets_detected
+
+  def update_weight_sensor_packets_detected(self):
+    self.weight_sensor_packets_detected = self.weight_sensor_packets_detected + 1
+
+  def get_weight_sensor_packet_detected(self):
+    return self.weight_sensor_packet_detected
+
+  def update_arm_sensor_packets_detected(self):
+    self.arm_sensor_packets_detected = self.arm_sensor_packets_detected + 1
+
+  def get_arm_sensor_packet_detected(self):
+    return self.arm_sensor_packet_detected
+
+app.SorterService = Sorter()
--- CoreService.py/app/views.py
+++ CoreService.py/app/views.py
@@ -1,25 +1,62 @@
 from app import app
 import requests
 
+sorter = SorterService()
+
 @app.route('/')
 @app.route('/index')
 def index():
   return "Core Service"
 
-@app.route('/camera_sensor')
+@app.route('/camera_sensor', methods=['POST'])
 def camera_sensor():
+  packet = Packet()
+  sorter.add_packet(packet)
+  packet_id = sorter.get_camera_sensor_packets_detected()
+  sorter.update_camera_sensor_packets_detected()
   url = app.config['CONNECTION_PROTOCOL']+app.config['BARCODE_SERVICE_URL']+app.config['BARCODE_SERVICE_PORT']+app.config['BARCODE_SERVICE_ENDPOINT']
   headers = {}
-  return str((requests.get(url, headers=headers)).json())
+  data = {'packet_id':packet_id}
+  return requests.post(url, headers=headers, data=data)
 
-@app.route('/weight_sensor')
+@app.route('/barcode', methods=['POST'])
+def set_barcode():
+  packet_id = requests.json['packet_id']
+  barcode = requests.json['barcode']
+  packet = sorter.get_packet(packet_id)
+  packet.barcode = barcode
+  sorter.update_packet(packet_id, packet)
+
+@app.route('/weight_sensor', methods=['POST'])
 def weight_sensor():
+  packet_id = sorter.get_weight_sensor_packets_detected()
+  sorter.update_weight_sensor_packets_detected()
   url = app.config['CONNECTION_PROTOCOL']+app.config['WEIGHT_SERVICE_URL']+app.config['WEIGHT_SERVICE_PORT']+app.config['WEIGHT_SERVICE_ENDPOINT']
   headers = {}
-  return str((requests.get(url, headers=headers)).json())
+  data = {'packet_id':packet_id}
+  return requests.post(url, headers=headers, data=data)
 
-@app.route('/arm_sensor')
+@app.route('/weight', methods=['POST'])
+def set_weight():
+  packet_id = requests.json['packet_id']
+  weight = requests.json['weight']
+  packet = sorter.get_packet(packet_id)
+  packet.weight = weight
+  sorter.update_packet(packet_id, packet)
+
+@app.route('/arm_sensor', methods=['POST'])
 def arm_sensor():
+  packet_id = sorter.get_arm_sensor_packets_detected()
+  sorter.update_arm_sensor_packets_detected()
   url = app.config['CONNECTION_PROTOCOL']+app.config['ARM_SERVICE_URL']+app.config['ARM_SERVICE_PORT']+app.config['ARM_SERVICE_ENDPOINT']
   headers = {}
-  return str((requests.get(url, headers=headers)).json())
+  data = {'packet_id':packet_id}
+  return requests.post(url, headers=headers, date=data)
+
+@app.route('/arm', methods=['POST'])
+def set_arm():
+  packet_id = requests.json['packet_id']
+  arm = requests.json['arm']
+  packet = sorter.get_packet(packet_id)
+  packet.arm = arm
+  sorter.update_packet(packet_id, packet)
